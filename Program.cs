a386ad3bf2d0765fffd515d2cf289802d3fcbc33
Mohamed Fathy <codetwostart@gmail.com>
2019-10-06 18:55:52 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace implicitandexplicitconversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int f1 = 232;
+            float f2 = f1;//implicit conversion
+            Console.WriteLine(f2);
+            int a1 = 12;
+            byte b1 = (byte)a1;//Exciplcit conversion
+            Console.WriteLine(b1);
+            
+        }
+    }
+}
